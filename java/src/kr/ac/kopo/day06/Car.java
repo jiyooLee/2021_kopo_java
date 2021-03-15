c8ced25e637bada5b44bcb6fd325bed85827c53d
jiyoo <iamjiyoo.kopo@gmail.com>
2021-03-15 15:44:34 +0900

    3월 15일 수업내용-최종

---
+++ java/src/kr/ac/kopo/day06/Car.java
@@ -0,0 +1,9 @@
+package kr.ac.kopo.day06;
+
+// 참조클래스
+public class Car {
+
+	String 	name;
+	int 	price;
+	String 	company;
+}
---
+++ java/src/kr/ac/kopo/day06/Handphone.java
@@ -0,0 +1,8 @@
+package kr.ac.kopo.day06;
+
+public class Handphone {
+
+	String name;	/** 소유주명 */
+	String phone;	/** 핸드폰번호 */
+	String company;	/** 핸드폰 제조사 */
+}
